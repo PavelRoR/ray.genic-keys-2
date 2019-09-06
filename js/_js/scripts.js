c03608987032ad91bcfad3671e4d6a1e8bacf884
pavelp21 <pavelvp2182@yandex.ru>
2019-09-07 00:22:30 +0500

    Modal is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -3,7 +3,68 @@
 //@prepros-prepend jquery.fancybox.min.js
 
 $(document).ready(function () {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            button = $('.button-form', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                reNone = /.+/,
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+            if (!email.val().match(reNone)) {
+                button.css({
+                    'fontSize': '16px',
+                    'color': 'red'
+                }).text('Введите email');
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                button.css({
+                    'fontSize': '16px',
+                    'color': 'red'
+                }).text('Соглашение!');
+                return false;
+            }
+            if (email.val() && check) {
 
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            button.css({
+                'fontSize': '28px',
+                'color': 'rgb(42, 45, 62)'
+            }).text('Записаться');
+            // message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#aaa",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#aaa",
+                'transition': 'all .4s ease'
+            });
+            button.css({
+                'fontSize': '28px',
+                'color': 'rgb(42, 45, 62)'
+            }).text('Записаться');
+        });
+    });
 
     $(".video-wrapper-revs img").click(function () {
         var a = $(this).parent().attr("data-youtube");

c656cf198a1ccfe25bd35f69145809319fdb1e42
pavelp21 <pavelvp2182@yandex.ru>
2019-09-06 20:47:29 +0500

    Section Revs is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -5,6 +5,21 @@
 $(document).ready(function () {
 
 
-
+    $(".video-wrapper-revs img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>');
+    });
+    $(function () {
+        $('#video-revs-slider').on('slide.bs.carousel', function () {
+            $('.video-wrapper-revs iframe').each(function () {
+                var l = $(this).parent().attr('data-img');
+                $(this).parent().html('<img class="video-rev-img" src="' + l + '" alt="Видео отзыв">');
+            });
+            $(".video-wrapper-revs img").click(function () {
+                var a = $(this).parent().attr("data-youtube");
+                $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>');
+            });
+        });
+    });
     /*Конец документа*/
 });
